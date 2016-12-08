0422b506e755fc116e070eb89909edf83e149c47
margaritakop <mbk11@ic.ac.uk>
2016-12-08 14:48:49 +0000

    161205 DNAConc

--- data_analysis.py
+++ data_analysis.py
@@ -6,12 +6,12 @@ import openpyxl
 #replicates
 r = 3
 #samples, the row is shifted by 13 in the excel output
-m = 111
+m = 39
 welln = m + 13
 #define the names for the conditions and DNA. this will be input on the interface at the 1st step.
 
 CONDNAMES = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
-DNANAMES = ['ThermoPrep', 'Midi', 'PrecMidi']
+DNANAMES = ['ThermoPrep']
 
 wb = openpyxl.load_workbook('data.xlsx')
 sheets = wb.get_sheet_names()
@@ -27,10 +27,15 @@ for i in DNANAMES:
         names.append(name)
         name = i
 
+print(names)
+
 #read in the dataand calculate the basal level from blanks
 rawdata = []
 for i in range(13, welln):
        rawdata.append((sheet.cell(row=i, column=4).value))
+
+print (rawdata)
+
 blanks = []
 x = 0
 for i in range(r):
